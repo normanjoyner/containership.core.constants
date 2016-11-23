ac16ad5e753dcb8eab0c8df0d5aaa509d2b9e9e3
Norman Joyner <norman.joyner@gmail.com>
2016-11-22 21:14:47 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const constants = {
+    myriad: {
+        APPLICATIONS: ['containership', 'application', '*'],
+        APPLICATION_PREFIX: ['containership', 'application'],
+        CLUSTER_ID: ['containership', 'cluster', 'id'],
+        CONTAINERS: ['containership', 'containers', '*'],
+        CONTAINERS_PREFIX: ['containership', 'containers'],
+        DELIMITER: '::',
+        ENOKEY: 'ENOKEY',
+        VARIABLES: ['containership', 'variables', '*'],
+        VARIABLES_PREFIX: ['containership', 'variables'],
+        TIDE_JOBS: ['containership', 'tide-jobs']
+    },
+
+    events: {
+        CLUSTER_ID: 'containership.cluster_id',
+        RECONCILE: 'containership.reconcile',
+        LOAD_CONTAINER: 'containership.load_container',
+        UNLOAD_CONTAINER: 'containership.unload_container',
+        UPDATE_HOST: 'containership.update_host',
+        DELETE_HOST: 'containership.delete_host'
+    }
+};
+
+for(const key in constants.myriad) {
+    if(constants.myriad[key] instanceof Array) {
+        constants.myriad[key] = constants.myriad[key].join(constants.myriad.DELIMITER);
+    }
+}
+
+module.exports = constants;
